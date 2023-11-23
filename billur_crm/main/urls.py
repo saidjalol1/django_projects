81dbf7b71e500ab05a8a473de2ef61cb60fbed32
Saidjalol Turakhujayev <turakhujayevsaidjalol06@gmail.com>
2023-11-23 08:52:42 +0500

    profile_added

--- billur_crm/main/urls.py
+++ billur_crm/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import StatisticsView
+from .views import StatisticsView, Customers, CustomersProfile
 
 
 app_name = 'main'
 urlpatterns = [
     path('', StatisticsView.as_view(), name='statistics'),
+    path('customers/', Customers.as_view(),name='customers' ),
+    path('customers/profile/', CustomersProfile.as_view(), name='profile')
 ]
--- billur_crm/main/views.py
+++ billur_crm/main/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 
 class StatisticsView(TemplateView):
     template_name = 'index.html'
+
+
+class Customers(TemplateView):
+    template_name = 'customers/ecom-customers.html'
+
+
+class CustomersProfile(TemplateView):
+    template_name = 'customers/customers_profile.html'
